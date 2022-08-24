40fbca319a18ea550b7f2070b4c0b4ace840814b
Emmanuel Elias <31974716+mcjovial@users.noreply.github.com>
2022-08-24 02:25:14 +0100

    Update distinct.js

--- 06-sorting/distinct.js
+++ 06-sorting/distinct.js
@@ -1,9 +1,8 @@
 function solution(A) {
-  const N = A.length;
   const map = {};
 
-  for (let i = 0; i < N; i++) {
-    map[A[i]] = true;
+  for (i of A) {
+    map[i] = true;
   }
 
   return Object.keys(map).length;
